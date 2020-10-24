7cf72b1cc9dfa598499633df89cb2200ee389d4c
diegopagini <diegopagini777@gmail.com>
2020-10-24 06:32:42 -0300

    añadido el selection sort

--- ordenamiento.js
+++ ordenamiento.js
@@ -8,14 +8,20 @@ class BubbleSort {
   }
 
   sort() {
+    let swapped = false;
     for (let i = 0; i < this.array.length; i++) {
+      swapped = false;
       for (let j = 0; j < this.array.length; j++) {
         if (this.array[j] > this.array[j + 1]) {
           this.buffer = this.array[j];
           this.array[j] = this.array[j + 1];
           this.array[j + 1] = this.buffer;
+          swapped = true;
         }
         // console.log(this.array);
+      }
+      if (swapped === false) {
+        break;
       }
     }
   }
@@ -30,41 +36,44 @@ const littleArray = [2, 1, 3];
 let temporal;
 
 function bubleSort(theArray) {
+  let swapped = false;
   //Hago dos bluces para que recorran todo el array y comparen
   for (let i = 0; i < theArray.length; i++) {
+    swapped = false;
     for (let j = 0; j < theArray.length; j++) {
-      //Si el primer item que estoy comparando es mayor al segundo: guardo en una variable temporal el primer valor, el primer valor lo guardo donde se econtraba el segundo y el segundo pasa a estar en el lugar de tempora.
+      //Si el primer item que estoy comparando es mayor al segundo: guardo en una variable temporal el primer valor, el primer valor lo guardo donde se econtraba el segundo y el segundo pasa a estar en el lugar de temporal.
       if (theArray[j] > theArray[j + 1]) {
         temporal = theArray[j];
-        console.log(
-          "temporal",
-          temporal,
-          "thearray[j]",
-          theArray[j],
-          "thearray[j + 1]",
-          theArray[j + 1]
-        ); // En este momento temporal es = 2, theArray[j] = 2 y theArray[j + 1] = 1
         theArray[j] = theArray[j + 1];
-        console.log(
-          "temporal",
-          temporal,
-          "thearray[j]",
-          theArray[j],
-          "thearray[j + 1]",
-          theArray[j + 1]
-        ); // En este momento temporal es = 2, theArray[j] = 1 y theArray[j + 1] = 1
         theArray[j + 1] = temporal;
-        console.log(
-          "temporal",
-          temporal,
-          "thearray[j]",
-          theArray[j],
-          "thearray[j + 1]",
-          theArray[j + 1]
-        ); // En este momento temporal es = 2, theArray[j] = 2 y theArray[j + 1] = 2
+        swapped = true;
       }
-      console.log(theArray); // Queda ordenado como 1,2,3
+    }
+    if (swapped === false) {
+      break;
     }
   }
 }
 bubleSort(littleArray);
+console.log(littleArray);
+bubleSort(myArray);
+console.log(myArray);
+
+//Selection Sort
+function selectionSort(arr) {
+  let aux = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let indMin = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[indMin] > arr[j]) {
+        indMin = j;
+      }
+    }
+    aux = arr[i];
+    arr[i] = arr[indMin];
+    arr[indMin] = aux;
+  }
+}
+
+selectionSort(myArray);
+console.log("Selection Sort", myArray);
